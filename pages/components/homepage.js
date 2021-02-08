7461caeb605a7a7da9265962c2afd526782f10fa
Adamu M. Dankore <zimmazone@yahoo.com>
2021-02-07 21:26:30 -0600

    Update template

--- pages/components/homepage.js
+++ pages/components/homepage.js
@@ -162,7 +162,7 @@ export default function Homepage(props) {
             <div className="sm:text-center lg:text-left">
               <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
                 <span className="block xl:inline">Next.js MongoDB </span>
-                <span className="block text-indigo-600 xl:inline">Starter Template!</span>
+                <span className="block text-indigo-600 xl:inline">Starter Template</span>
               </h1>
               <p className="my-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
                 Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat
